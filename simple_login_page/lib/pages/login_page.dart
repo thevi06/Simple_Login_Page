5a0758d5895f7494847b9e97049d364cfda5fe59
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-09-11 14:40:41 +0530

    Update login_page.dart

--- simple_login_page/lib/pages/login_page.dart
+++ simple_login_page/lib/pages/login_page.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+    );
+  }
+}
