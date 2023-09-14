86ec0874741e3bbd36629295d7f4bac59ef43b64
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-09-14 10:17:33 +0530

    Update login_page.dart

--- simple_login_page/lib/pages/login_page.dart
+++ simple_login_page/lib/pages/login_page.dart
@@ -50,6 +50,20 @@ class LoginPage extends StatelessWidget {
               ),
 
               const SizedBox(height: 10),
+
+              //forgot password?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
     );
   }
 }
