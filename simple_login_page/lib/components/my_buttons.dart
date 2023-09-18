af378f8ddd46e1182ef6aad6760afa736c712e60
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-09-18 10:14:29 +0530

    import dependencies and update my_buttons.dart

--- simple_login_page/lib/components/my_buttons.dart
+++ simple_login_page/lib/components/my_buttons.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
+
+class MyButton extends StatelessWidget {
+  final Function()? onTap;
+
+  const MyButton({Key? key, required this.onTap}) : super(key: key);
+
+  _showSuccessDialog(BuildContext context) {
+    // Delayed navigation to the second page after showing the dialog
+    // Future.delayed(const Duration(seconds: 2), () {
+    //   Navigator.of(context).pushReplacement(MaterialPageRoute(
+    //     builder: (context) => const SecondPage(),
+    //   ));
+    // });
