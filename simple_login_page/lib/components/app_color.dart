cab9f76af39107ecd6577e9361fdaac141332c39
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-09-25 14:43:20 +0530

    Update app_color.dart

--- simple_login_page/lib/components/app_color.dart
+++ simple_login_page/lib/components/app_color.dart
@@ -1,1 +1,11 @@
 import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color primaryColor = const Color(0xFF1C3E66);
+  static Color secondaryColor = const Color.fromARGB(255, 3, 8, 104);
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF000000);
+  static Color blackGrey = const Color.fromARGB(255, 39, 39, 39);
+  static Color backgroundBlack = const Color.fromARGB(255, 37, 37, 37);
+  static Color backgroundWhite = const Color.fromARGB(255, 255, 255, 255);
+  static Color backgroundGray = const Color.fromARGB(255, 240, 240, 240);
