e6e84e6123e1863ed0990c89075e7d08e67c98e1
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-10-03 08:41:01 +0530

    Update my_textfield.dart

--- simple_login_page/lib/components/my_textfield.dart
+++ simple_login_page/lib/components/my_textfield.dart
@@ -4,3 +4,9 @@ class MyTextField extends StatelessWidget {
   final controller;
   final String hintText;
   final bool obscureText;
+
+  const MyTextField(
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      required this.obscureText});
