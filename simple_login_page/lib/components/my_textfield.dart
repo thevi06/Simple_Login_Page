578c7fc692eebd418dd09a236e5200c96a3c1a22
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-10-03 08:41:53 +0530

    Update my_textfield.dart

--- simple_login_page/lib/components/my_textfield.dart
+++ simple_login_page/lib/components/my_textfield.dart
@@ -14,4 +14,11 @@ class MyTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),    
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),    
