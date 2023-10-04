ad89f439b400a2dc8328fb640b89b2193b6928db
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-10-04 14:21:06 +0530

    Update app_theme.dart

--- simple_login_page/lib/pages/app_theme.dart
+++ simple_login_page/lib/pages/app_theme.dart
@@ -30,3 +30,29 @@ class AppTheme {
       bodySmall: _textStyle.bodySmall,
     ),
   );
+
+  static ThemeData dark = ThemeData(
+    brightness: Brightness.dark,
+    backgroundColor: AppColor.backgroundBlack,
+    scaffoldBackgroundColor: AppColor.backgroundBlack,
+    primaryColor: AppColor.primaryColor,
+    primarySwatch: AppColor.primarySwatch,
+    textTheme: TextTheme(
+      displayLarge: _textStyle.displayLarge.copyWith(color: AppColor.white),
+      displayMedium: _textStyle.displayMedium.copyWith(color: AppColor.white),
+      displaySmall: _textStyle.displaySmall.copyWith(color: AppColor.white),
+      headlineLarge: _textStyle.headlineLarge.copyWith(color: AppColor.white),
+      headlineMedium: _textStyle.headlineMedium.copyWith(color: AppColor.white),
+      headlineSmall: _textStyle.headlineSmall.copyWith(color: AppColor.white),
+      titleLarge: _textStyle.titleLarge.copyWith(color: AppColor.white),
+      titleSmall: _textStyle.titleSmall.copyWith(color: AppColor.white),
+      titleMedium: _textStyle.titleMedium.copyWith(color: AppColor.white),
+      labelLarge: _textStyle.labelLarge.copyWith(color: AppColor.white),
+      labelMedium: _textStyle.labelMedium.copyWith(color: AppColor.white),
+      labelSmall: _textStyle.labelSmall.copyWith(color: AppColor.white),
+      bodyLarge: _textStyle.bodyLarge.copyWith(color: AppColor.white),
+      bodyMedium: _textStyle.bodyMedium.copyWith(color: AppColor.white),
+      bodySmall: _textStyle.bodySmall.copyWith(color: AppColor.white),
+    ),
+  );
+}
