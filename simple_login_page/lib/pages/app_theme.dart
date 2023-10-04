7cb3d41b1eb62305a37bb7cf160f8e0fb74d6495
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-10-04 14:20:49 +0530

    Update app_theme.dart

--- simple_login_page/lib/pages/app_theme.dart
+++ simple_login_page/lib/pages/app_theme.dart
@@ -2,3 +2,31 @@ import 'package:flutter/material.dart';
 import '../components/app_color.dart';
 
 import '../components/app_textstyle.dart';
+
+class AppTheme {
+  static final AppTextStyle _textStyle = AppTextStyle.instance;
+
+  static ThemeData light = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: AppColor.primaryColor,
+    backgroundColor: AppColor.backgroundWhite,
+    scaffoldBackgroundColor: AppColor.backgroundWhite,
+    primarySwatch: AppColor.primarySwatch,
+    textTheme: TextTheme(
+      displayLarge: _textStyle.displayLarge,
+      displayMedium: _textStyle.displayMedium,
+      displaySmall: _textStyle.displaySmall,
+      headlineLarge: _textStyle.headlineLarge,
+      headlineMedium: _textStyle.headlineMedium,
+      headlineSmall: _textStyle.headlineSmall,
+      titleLarge: _textStyle.titleLarge,
+      titleSmall: _textStyle.titleSmall,
+      titleMedium: _textStyle.titleMedium,
+      labelLarge: _textStyle.labelLarge,
+      labelMedium: _textStyle.labelMedium,
+      labelSmall: _textStyle.labelSmall,
+      bodyLarge: _textStyle.bodyLarge,
+      bodyMedium: _textStyle.bodyMedium,
+      bodySmall: _textStyle.bodySmall,
+    ),
+  );
