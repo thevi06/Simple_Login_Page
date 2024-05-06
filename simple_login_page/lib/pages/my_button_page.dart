89ddd9dff9cd44674f5de5e9431af1dff5258cdb
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-05-06 11:06:45 +0530

    Update my_button_page.dart

--- simple_login_page/lib/pages/my_button_page.dart
+++ simple_login_page/lib/pages/my_button_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:simple_login_page/components/my_buttons.dart';
-import 'second_page.dart'; // Import the second page file
-//import 'my_buttons.dart'; // Import the MyButton widget file
+// import 'package:flutter/material.dart';
+// import 'package:simple_login_page/components/my_buttons.dart';
+// import 'second_page.dart'; // Import the second page file
+// import 'my_buttons.dart'; // Import the MyButton widget file
